9f0cc3656c5d3a81446fdfe37245282cc6082692
Lillian Armes <lily@hotgirl.zone>
2022-02-12 17:50:37 -0500

    Better eventbus testing

--- common/src/test/kotlin/com/deltaclient/common/event/EventBusTest.kt
+++ common/src/test/kotlin/com/deltaclient/common/event/EventBusTest.kt
@@ -3,16 +3,22 @@ package com.deltaclient.common.event
 import com.deltaclient.common.event.annotation.Subscribe
 import org.junit.jupiter.api.Test
 import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 import java.util.function.Consumer
 
 internal class EventBusTest {
     @Test
     fun testSubscribe() {
-        val latch = CountDownLatch(2)
+        val latch = CountDownLatch(3)
         val sub = Sub(latch)
 
         EventBus.subscribe(sub)
+        EventBus.subscribe<Int> {
+            latch.countDown()
+        }
+
         EventBus.post(1)
+        latch.await(50, TimeUnit.MILLISECONDS)
     }
 
     class Sub(private val latch: CountDownLatch) {
